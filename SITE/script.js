8469252696f3a8c653b251cbbf5b5c383df71486
Alyson-Vinicius <avgs1@discente.ifpe.edu.br>
2024-05-08 18:20:51 -0300

    adicionando o loading

--- SITE/script.js
+++ SITE/script.js
@@ -1,79 +1,82 @@
 function onChangeEmail() {
-	toggleButtonsDisable();
-	toggleEmailErros();
+  toggleButtonsDisable();
+  toggleEmailErros();
 }
 
 function onChangePassword() {
-	toggleButtonsDisable();
-	togglePassworErro();
+  toggleButtonsDisable();
+  togglePassworErro();
 }
 
 function login() {
-	firebase.auth().signInWithEmailAndPassword(
-		form.email().value, form.password().value).then(Response => {
-			window.location.href = "pages/HOME/home.html";
-		}).catch(error => {
-			alert(getErroMessage(error));
-		});
-
+	showLoading();
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(form.email().value, form.password().value)
+    .then((Response) => {
+		hideLoading();
+      window.location.href = "pages/HOME/home.html";
+    })
+    .catch((error) => {
+		hideLoading();
+      alert(getErroMessage(error));
+    });
 }
 
 function getErroMessage(error) {
-	if (error.code == "auth/user-not-found") {
-		return "usuário não encontrado"
-	}
-	return error.message;
+  if (error.code == "auth/user-not-found") {
+    return "usuário não encontrado";
+  }
+  return error.message;
 }
 
 function register() {
-	window.location.href = "pages/Registrar/register.html";
+  window.location.href = "pages/Registrar/register.html";
 }
 
 function isEmailValid() {
-	const email = form.email().value;
-	if (!email) {
-		return false;
-	}
-	return validateEmail(email);
+  const email = form.email().value;
+  if (!email) {
+    return false;
+  }
+  return validateEmail(email);
 }
 
 function toggleEmailErros() {
-	const email = form.email().value;
-    emailRequeridErro().style.display = email ? "block" : "none";
-	
-	emailInvalidErro().style.display = validateEmail(email) ? "none" : "block";
+  const email = form.email().value;
+  emailRequeridErro().style.display = email ? "block" : "none";
+
+  emailInvalidErro().style.display = validateEmail(email) ? "none" : "block";
 }
 
 function togglePassworErro() {
-	const password = form.password().value;
+  const password = form.password().value;
 
-	passwordRequeridErro().style.display = password ? "block" : "none";
+  passwordRequeridErro().style.display = password ? "block" : "none";
 }
-
 
 function toggleButtonsDisable() {
-	const emailvalid = isEmailValid();
-	form.recoverPassword().disabled = !emailvalid;
+  const emailvalid = isEmailValid();
+  form.recoverPassword().disabled = !emailvalid;
 
-	const Passwordvalid = isPasswordvalid();
-	form.loginButton().disabled = !emailvalid || !Passwordvalid;
+  const Passwordvalid = isPasswordvalid();
+  form.loginButton().disabled = !emailvalid || !Passwordvalid;
 }
 
-
 function isPasswordvalid() {
-	const password = form.password().value;
-	if (!password) {
-		return false;
-	}
-	return true;
+  const password = form.password().value;
+  if (!password) {
+    return false;
+  }
+  return true;
 }
 
 const form = {
-	email: () => document.getElementById('email'),
-	emailInvalidErro: () => document.getElementById('email-invalid-erro'),
-	emailRequeridErro: () => document.getElementById('email-requerid-erro'),
-	loginButton: () => document.getElementById('login-button'),
-	password: () => document.getElementById('password'),
-	passwordRequeridErro: () => document.getElementById('password-requerid-erro'),
-	recoverPassword: () => document.getElementById('forgot-password'),
-}
+  email: () => document.getElementById("email"),
+  emailInvalidErro: () => document.getElementById("email-invalid-erro"),
+  emailRequeridErro: () => document.getElementById("email-requerid-erro"),
+  loginButton: () => document.getElementById("login-button"),
+  password: () => document.getElementById("password"),
+  passwordRequeridErro: () => document.getElementById("password-requerid-erro"),
+  recoverPassword: () => document.getElementById("forgot-password"),
+};
