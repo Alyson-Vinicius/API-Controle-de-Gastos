b007dc224469ddcee68abce7d0f7195c72ee5b50
Alyson-Vinicius <avgs1@discente.ifpe.edu.br>
2024-07-18 22:15:33 -0300

    atualização firebase

--- SITE/script/firebase-init.js
+++ SITE/script/firebase-init.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCzVpUgjAAIp68Auyc3EauDzSyc86RcNsU",
-    authDomain: "controle-de-gastos-22898.firebaseapp.com",
-    projectId: "controle-de-gastos-22898",
-    storageBucket: "controle-de-gastos-22898.appspot.com",
-    messagingSenderId: "23126576368",
-    appId: "1:23126576368:web:7dba7bea61e162d203c635"
-  };
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyCzVpUgjAAIp68Auyc3EauDzSyc86RcNsU",
+  authDomain: "controle-de-gastos-22898.firebaseapp.com",
+  projectId: "controle-de-gastos-22898",
+  storageBucket: "controle-de-gastos-22898.appspot.com",
+  messagingSenderId: "23126576368",
+  appId: "1:23126576368:web:7dba7bea61e162d203c635"
+};
+firebase.initializeApp(firebaseConfig);
